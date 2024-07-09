70359f09418efc55964eed9730c9b9628d81b981
Thoriq <thoriqaulia163@gmail.com>
2024-07-09 11:09:29 +0700

    initialize feldora project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './global.css'
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+
+function App() {
+  return (
+    <>
+      Feldora Project
+      <AccountBoxIcon/>
+    </>
+  )
+}
+
+export default App
