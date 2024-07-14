16afe185ea39d9b601a870d8e42b49b1a40bb40b
Thoriq <thoriqaulia163@gmail.com>
2024-07-14 15:30:24 +0700

    fix naming error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './global.css'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
-import Home from '@pages/Home/Home'
-import Search from '@pages/Search/Search'
-import About from '@pages/About/About'
+import Home from '@pages/home/Home'
+import Search from '@pages/search/Search'
+import About from '@pages/about/About'
 
 function App() {
   return (
