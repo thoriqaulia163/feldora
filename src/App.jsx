b00a3557cc2401b31c1d53c49c65290eb5373c7d
Thoriq <thoriqaulia163@gmail.com>
2024-07-14 15:25:44 +0700

    fixing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './global.css'
 import { Routes, Route } from 'react-router-dom'
-import Navbar from '@components/Navbar/Navbar'
+import Navbar from './components/navbar/Navbar'
 import Home from '@pages/Home/Home'
 import Search from '@pages/Search/Search'
 import About from '@pages/About/About'
